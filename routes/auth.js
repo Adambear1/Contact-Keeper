fda7fc8893b93069e7ec699d55b9ae3f0a305e1e
Adam Birgenheier <adamcarlbirgenheier@gmail.com>
2020-07-16 09:00:32 -0700

    updated backend

--- routes/auth.js
+++ routes/auth.js
@@ -10,8 +10,14 @@ const { check, validationResult } = require("express-validator/check");
 // @route   GET api/auth
 // @desc    GET logged in user
 // @access  Private
-router.get("/", auth, (req, res) => {
-  res.send();
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server Error");
+  }
 });
 
 // @route   POST api/auth
